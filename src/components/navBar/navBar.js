1702e4a8e703d6c08b8a8741714114b767f9c662
idriss30 <idriss.cissoko@hotmail.com>
2020-12-22 23:09:46 -0500

    navigation made responsive

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -3,24 +3,54 @@ import logo from "../../svgs/logo.svg";
 import "./navBar.scss";
 import React from "react";
 
+const Links = ({ styleName }) => {
+  return menuItems.map((item) => (
+    <li className={styleName} key={item.title}>
+      <a href={item.link}>{item.title}</a>
+    </li>
+  ));
+};
+
 class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggle: false,
+    };
+  }
+  handleClick = () => {
+    this.setState({ toggle: !this.state.toggle });
+  };
+
   render() {
     return (
       <header>
         <nav className="nav__bar">
           <div className="nav__bar__container">
             <div className="nav__bar-logo">
-              <a href="#">
+              <a href="/home">
                 <img src={logo} alt="logo" name="logo" />
               </a>
             </div>
-            <ul>
-              {menuItems.map((item) => (
-                <li key={item.title}>
-                  <a href={item.link}>{item.title}</a>
-                </li>
-              ))}
+            <ul className="nav__bar__list-desktop">
+              <Links styleName="nav__bar-links" />
             </ul>
+            <div className="nav__bar__mobile">
+              <button onClick={this.handleClick}>
+                <i
+                  className={this.state.toggle ? "fas fa-times" : "fas fa-bars"}
+                ></i>
+              </button>
+              <div>
+                <ul className="nav__bar__list-mobile">
+                  {this.state.toggle ? (
+                    <Links styleName="nav__bar-links-mobile" />
+                  ) : (
+                    ""
+                  )}
+                </ul>
+              </div>
+            </div>
           </div>
         </nav>
       </header>
